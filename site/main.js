d828dc7e6fde1c78238a86c9337538902bea4345
iamtechboy2 <fabypiere@gmail.com>
2020-05-01 16:40:20 -0400

    Js modification.

--- site/main.js
+++ site/main.js
@@ -6,26 +6,3 @@ if (confirm('Please click "OK" if you want to have access to this website '))
 	alert('Sorry your access request has been denied');
 }
 
-var message = 'This is a global value';
-function showMsg(){
-	var message = 'And now the local one';
-	alert(message);
-}
-showMsg();
-alert(message);
-
-function myFunction(arg){
-	alert('Your argument : ' + arg);
-}
-myFunction(prompt('What would would like to say to the function?'));
-
-function sayHello(){
-	return 'Hello!';
-	alert('Watch Out!');
-}
-alert(sayHello());
-
-var sayHello = (function(){
-	return 'Yup';
-})();
-alert(sayHello);
